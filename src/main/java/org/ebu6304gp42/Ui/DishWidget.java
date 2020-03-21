52f2262b8a8a30a4961679483469670598d95d49
ForeverCyril <cyrilchen1999@gmail.com>
2020-03-21 23:27:01 +0800

    Finish Dish Widget

---
+++ src/main/java/org/ebu6304gp42/Ui/DishWidget.java
@@ -0,0 +1,88 @@
+package org.ebu6304gp42.Ui;
+
+import org.ebu6304gp42.Config.PathConfig;
+import org.ebu6304gp42.Data.Dish;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.effect.BlurType;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.Label;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class DishWidget extends HBox {
+    final static InputStream default_pic = DishWidget.class.getResourceAsStream(PathConfig.DEFAULT_PIC);
+    final static double HEIGHT = 120;
+
+    private Dish dish;
+
+    public DishWidget(Dish dish){
+        this.dish = dish;
+        ImageView image_view;
+
+        this.setHeight(HEIGHT);
+        this.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
+
+        Image image;
+        try {
+            image = new Image(new FileInputStream(dish.getPic()));
+        } catch (FileNotFoundException e){
+            image = new Image(default_pic);
+        }
+        image_view = new ImageView(image);
+        image_view.preserveRatioProperty().setValue(false);
+        image_view.fitWidthProperty().bind(this.heightProperty());
+        image_view.fitHeightProperty().bind(this.heightProperty());
+
+
+        Label name = new Label(dish.getName());
+        name.setFont(Font.font(null, FontWeight.BOLD, 14));
+        name.setTextFill(Color.BLACK);
+
+        Label price = new Label(String.format("%.2f $", dish.getPrice()));
+        price.setFont(Font.font(null,FontWeight.BOLD, 14));
+        price.setTextFill(Color.RED);
+
+        Label description = new Label(dish.getDescription());
+        description.setTextFill(Color.GRAY);
+        description.setFont(Font.font(null,11));
+        description.setMaxHeight(HEIGHT);
+        description.setAlignment(Pos.TOP_LEFT);
+        VBox.setVgrow(description, Priority.ALWAYS);
+
+        VBox imformation_box = new VBox();
+        imformation_box.prefHeightProperty().bind(this.heightProperty());
+        imformation_box.setFillWidth(true);
+        imformation_box.setPadding(new Insets(5));
+        imformation_box.setSpacing(8);
+        imformation_box.setMaxWidth(HEIGHT*1.6);
+        imformation_box.getChildren().addAll(name,price, description,new Label("Recomened"));
+
+        description.setWrapText(true);
+        this.getChildren().addAll(image_view, imformation_box);
+
+        //Drop Shadow and it effect when mouse entering
+        DropShadow shadow = new DropShadow();
+        shadow.setBlurType(BlurType.GAUSSIAN);
+        shadow.setColor(Color.GRAY);
+        shadow.setSpread(0.1);
+        shadow.setOffsetX(1);
+        shadow.setOffsetY(1);
+        this.setEffect(shadow);
+        this.setOnMouseEntered(event -> {shadow.setSpread(0.5);});
+        this.setOnMouseExited(event -> {shadow.setSpread(0.1);});
+    }
+
+    public Dish getDish() {
+        return dish;
+    }
+}
---
+++ src/main/java/org/ebu6304gp42/Ui/Mainwindow.java
@@ -0,0 +1,28 @@
+package org.ebu6304gp42.Ui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+import org.ebu6304gp42.Data.Dish;
+
+
+public class Mainwindow extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FlowPane root = new FlowPane();
+        Scene scene = new Scene(root,400, 300);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        Dish dish = new Dish();
+        dish.setName("Test Name");
+        dish.setDescription("Tiosfj jdflas jfdsk ghdfhj fhda kafopjf fjid fa nffa;jf");
+        dish.setPic("Res/pic/dish/type1.png");
+        dish.setPrice(12.9887);
+        DishWidget dishWidget = new DishWidget(dish);
+        dishWidget.setLayoutX(10);
+        dishWidget.setLayoutY(10);
+        root.getChildren().add(dishWidget);
+    }
+}
