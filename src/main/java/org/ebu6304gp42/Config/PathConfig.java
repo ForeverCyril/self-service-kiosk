e6b8425b6312ecc58e2678661fd7185e5ffdf09a
ForeverCyril <cyrilchen1999@gmail.com>
2020-03-23 15:51:26 +0800

    Add Application Icon, and set Tiltle

--- src/main/java/org/ebu6304gp42/Config/PathConfig.java
+++ src/main/java/org/ebu6304gp42/Config/PathConfig.java
@@ -1,8 +1,10 @@
 package org.ebu6304gp42.Config;
 
+import java.io.File;
+
 public class PathConfig {
     public static String DISH_FILE = "Data/dish.json";
     public static String ACCOUNT_FILE = "Data/account.json";
     public static String ORDER_FILE = "Data/order.json";
-    public static String  DEFAULT_PIC = "/Res/pic/default_pic.png";
+    public static String  DEFAULT_PIC = "/res/pic/default_pic.png";
 }
