017fd0b5732e431b6a65dc214ce8e6a20da6ba1b
ForeverCyril <cyrilchen1999@gmail.com>
2020-04-17 21:54:08 +0800

    Rewrite into FXML: finish PaymentDialog

--- src/main/java/org/ebu6304gp42/View/ConfirmDialog.java
+++ src/main/java/org/ebu6304gp42/View/ConfirmDialog.java
@@ -1,6 +1,8 @@
 package org.ebu6304gp42.View;
 
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -14,8 +16,10 @@ import java.io.IOException;
 
 public class ConfirmDialog extends Dialog<Boolean> {
     private Order order;
+    private ConfirmController controller;
 
     public ConfirmDialog(Order order){
+        this.order = order;
         setTitle("Confirm Order");
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXML/Confirm.fxml"));
         try {
@@ -23,25 +27,44 @@ public class ConfirmDialog extends Dialog<Boolean> {
         } catch (IOException e){
             e.printStackTrace();
         }
-        ConfirmController controller = (ConfirmController)loader.getController();
+        controller = (ConfirmController)loader.getController();
         controller.setOrder(order);
         getDialogPane().setContent(loader.getRoot());
 
         getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
         Button ok_btn = (Button) getDialogPane().lookupButton(ButtonType.OK);
-        ok_btn.setOnAction(event -> {
-            Account account = controller.getLoginAccount();
-            if(account != null){
-                if(controller.isUseStamp()){
-                    account.useCount();
-                } else {
-                    account.addCount();
-                }
-            }
-            OrderManager.getInstance().addOrder(order);
-        });
+        ok_btn.addEventFilter(ActionEvent.ACTION, onPayment); // Handle the button action before dialog;
         setResultConverter(btn -> {
             return btn != ButtonType.CANCEL;
         });
+    }
+
+    /**
+     * Handle Ok Button Click Event before Dialog Close;
+     */
+    EventHandler<ActionEvent> onPayment = event -> {
+        System.out.println("Payment");
+        Account account = controller.getLoginAccount();
+        if(account == null){
+            var res = showPayment(order.getPrice());
+            if(!res){ event.consume();}
+        } else {
+            if(controller.isUseStamp()){
+                var res = showPayment(0);
+                if(!res){ event.consume();}
+                else {account.useCount();}
+            } else {
+                var res = showPayment(order.getPrice());
+                if(!res){ event.consume();} else {
+                    account.addCount();
+                }
+            }
+        }
+        OrderManager.getInstance().addOrder(order);
+    };
+
+    private boolean showPayment(double price){
+        var res = (new PaymentDialog(price)).showAndWait();
+        return res.orElse(false);
     }
 }
