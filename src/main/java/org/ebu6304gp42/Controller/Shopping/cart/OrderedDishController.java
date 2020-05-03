b6b8e68a682c762deb7f428f92b08d7710abab6d
ForeverCyril <cyrilchen1999@gmail.com>
2020-05-03 16:42:48 +0800

    Add OrderedDish Delete

--- src/main/java/org/ebu6304gp42/Controller/Shopping/cart/OrderedDishController.java
+++ src/main/java/org/ebu6304gp42/Controller/Shopping/cart/OrderedDishController.java
@@ -1,11 +1,20 @@
 package org.ebu6304gp42.Controller.Shopping.cart;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import org.ebu6304gp42.Data.OrderedDish;
 
-public class OrderedDishController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class OrderedDishController implements Initializable {
     private OrderedDish orderedDish;
 
     @FXML
@@ -34,5 +43,25 @@ public class OrderedDishController {
             opt_data.append(option.toString());
         }
         opt_label.setText(opt_data.toString());
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ContextMenu menu = new ContextMenu();
+        MenuItem mod_item = new MenuItem("Modify");
+        MenuItem del_item = new MenuItem("Delete");
+        del_item.setOnAction(event -> {
+            root.fireEvent(new OrderedDishDeleteEvent(OrderedDishDeleteEvent.DISH_DELETE_EVENT, orderedDish));
+        });
+
+        menu.getItems().addAll(mod_item, del_item);
+        root.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            // Show menu when right click or click + ctrl
+            if(event.getButton() == MouseButton.SECONDARY || event.isControlDown()){
+                menu.show(root, event.getScreenX(), event.getScreenY());
+            } else {
+                menu.hide();
+            }
+        });
     }
 }
